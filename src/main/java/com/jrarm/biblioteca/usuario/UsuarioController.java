71603514c0910871820e8dc90e35504132e7ec49
JRARM <77474292+JRARM@users.noreply.github.com>
2023-05-09 11:34:07 -0500

    model and script added

--- src/main/java/com/jrarm/biblioteca/usuario/UsuarioController.java
+++ src/main/java/com/jrarm/biblioteca/usuario/UsuarioController.java
@@ -3,6 +3,8 @@ package com.jrarm.biblioteca.usuario;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,5 +25,8 @@ public class UsuarioController {
     }
 
 
-
+    @PostMapping("/usuario")
+    public Usuario agregarUsuario(@RequestBody Usuario usuario){
+        return usuarioService.anadirUsuario(usuario);
+    }
 }
